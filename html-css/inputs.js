f34b36f127c6e1ff14516371d3cc7fbced547809
Chaithu-2007 <chaithu2809@gmail.com>
2026-06-12 15:15:37 +0530

    inputs

---
+++ html-css/inputs.js
@@ -0,0 +1,14 @@
+function greet_Hello() {
+    let text = document.getElementById("greet");
+    text.innerText = "Hello.............";
+    let hello=document.getElementById("dark");
+    hello.src="hello1.jpg"
+}
+function change_color() {
+    let color=document.getElementById("cool");
+    color.style.backgroundColor="violet";
+    let text1=document.getElementById("Data");
+    text1.style.color="hotpink";
+    let text2=document.getElementById("greet");
+    text2.style.color="skyblue";
+}  
--- html-css/manipulation.js
+++ html-css/manipulation.js
@@ -1,10 +1,15 @@
-let value=document.getElementById("wish").innerText;
+/*let value=document.getElementById("wish").innerText;
 console.log(value);
 let value1=document.getElementsByClassName("wishes");
 console.log(value1);
-let value2=document.getElementsByTagName("hi");
+let value2=document.getElementsByTagName("h1");
 console.log(value2);
 let value3=document.querySelector(".content");
 console.log(value3);
 let value4=document.querySelectorAll(".content");
 console.log(value4);
+let valuee=document.getElementById("wish");
+valuee.innerText="Happy Birthday@@@....";
+valuee.innerHTML="<h1><i><u>Happy Birthday🥰</u></i></h1>";
+valuee.style.fontsize="20px"; 
+valuee.style.fontstyle="bold";*/
